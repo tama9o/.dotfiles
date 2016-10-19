29e9635390b86d0982aba87e11507a16bfa89c53
tama9o <tama9o@hotmail.com>
2016-10-19 11:51:10 +0900

    Add setup script.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! `which curl` ]; then
+  echo curl command is not exist.
+  exit 1
+fi
+
+CONFIG_BASE=~/.dotfiles/
+
+curl -fLo $CONFIG_BASE/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo $CONFIG_BASE/.vim/colors/molokai.vim --create-dirs \
+  https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
