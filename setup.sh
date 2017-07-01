0e9f5e6b444b2ce4cf363a67da2d582b96976f92
tama9o <tama9o@hotmail.com>
2017-07-01 15:27:57 +0900

    Add a Vimperator's plugin for the Tree style tab.
    Firefox 54.0 with Vimperator, Tree Style Tab and Tab Mix Plus
    that has a promblem bellow:
    * New tab's position is become current tab's child
    
    This plugin is a work around it.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim: set ts=2 sw=2 expandtab :
 
 if [ ! `which curl` ]; then
   echo curl command is not exist.
@@ -13,3 +14,7 @@ curl -fLo $CONFIG_BASE/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo $CONFIG_BASE/.vim/colors/molokai.vim --create-dirs \
   https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+
+curl -fLo $CONFIG_BASE/.vimperator/plugin/tree-style-tab.js --create-dirs \
+  https://gist.githubusercontent.com/tama9o/97f573f6d15aae9317260d0346e28b93/raw/f18463fc176515c784eeaec78b588934db040977/tree-style-tab.js
+
